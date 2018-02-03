459ff488ca7a0203aae918ab9477af988d0fb3ea
daniel-unger <danunger24@gmail.com>
2018-02-03 17:23:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// write your code below!
+// write your code below!describe('functions', () => {
+/*  describe('happyHolidays', () => {
+    it('returns "Happy holidays!"', () => {
+      expect(happyHolidays()).toEqual('Happy holidays!')
+    })
+  })*/
+
+/*function functionName() {
+  return string.(string === string.toUpperCase)()
+}*/
+//if (string === string.toUpperCase)
+  //return "Happy holidays!"
+  function functionName() {
+    return  "Happy holidays!")
+   }
