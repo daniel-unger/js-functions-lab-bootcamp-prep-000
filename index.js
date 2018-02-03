0cf52ef37cdb61be2b37cf8570a459eb298af668
daniel-unger <danunger24@gmail.com>
2018-02-03 18:04:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,16 @@
 }*/
 //if (string === string.toUpperCase)
   //return "Happy holidays!"
-  function functionName() {
+1-  describe('functions', () => {
+    describe('happyHolidays', () => {
+      it('returns "Happy holidays!"', () => {
+        expect(happyHolidays()).toEqual('Happy holidays!')
+      })
+    })
+  2- function functionName() {
     return  "Happy holidays!")
    }
 
    Write a function named happyHolidays. This function should not accept any parameters and should return the string "Happy holidays!".
+   function myFunction(p1, p2) {
+       return p1 * p2;              // The function returns the product of p1 and p2
