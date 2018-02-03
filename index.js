7849367f7d3cf41411dc71077eefa597747e89d2
daniel-unger <danunger24@gmail.com>
2018-02-03 19:43:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,3 +59,14 @@ function happyHolidaysTo(name) {
      	function happyHolidayTo(holiday, name) {
        return `Happy ${holiday}, ${name}!`
      	}
+
+
+  /*    describe('holidayCountdown(holiday, days)', () => {
+        it('returns "It\'s ${days} days until ${holiday}!"', () => {
+          expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
+        })
+      })
+    })*/
+    function holidayCountdown(holiday, days) {
+  		 return `It's ${days} days until ${holiday}!`
+  	}
